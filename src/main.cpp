633cfbef1ef95f958eeac188ee5c0450afe25c8d
Alexandre Léonard <alex.dranoel@gmail.com>
2016-08-18 21:26:26 +0200

    Catchin the runtime error when not possible to connect to vme crate

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <qdebug.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
 
-    return a.exec();
+	 try {
+		MainWindow w;
+		w.show();
+		return a.exec();
+	 }
+	 catch (const std::runtime_error& err) {
+		qDebug() << err.what();
+		return 0;
+	 }
 }
